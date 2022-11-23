801c884f4c6bfe40c13d97d8b0f9d41ca4af0dbb
arthvrmichel <arthvrmichel@icloud.com>
2022-11-23 15:05:44 +0100

    Create Platform and Game database models

--- database/models.py
+++ database/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Platform(models.Model):
+    name = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=10)
+    
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    platform = models.ManyToManyField(Platform)
