8bac0567abe77719dcfe1f8d6029f07a23db1c20
arthvrmichel <arthvrmichel@icloud.com>
2022-11-25 11:48:26 +0100

    Add Version model

--- database/models.py
+++ database/models.py
@@ -7,10 +7,16 @@ class Platform(models.Model):
     
     def __str__(self) -> str:
         return self.name
+
+class Version(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self) -> str:
+        return self.name
     
 class Game(models.Model):
     name = models.CharField(max_length=200)
     platforms = models.ManyToManyField(Platform)
 
     def __str__(self) -> str:
-            return self.name
+        return self.name
